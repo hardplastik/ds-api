2b03c22d7d811a75d0b7c8b468da2a79a833aaf4
mauricio.cano <mauricio.cano>
2025-01-11 17:36:15 -0800

    Remove unused imports

--- src/main/java/io/hardplastik/ds/auth/SecurityConfig.java
+++ src/main/java/io/hardplastik/ds/auth/SecurityConfig.java
@@ -1,7 +1,5 @@
 package io.hardplastik.ds.auth;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
